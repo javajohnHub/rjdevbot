a8aabea4ca1f5c71727070dc76a6905c23145392
John Edwards <joedw1978@gmail.com>
2017-08-09 20:59:35 +0000

    Testing php command

--- modules/php.js
+++ modules/php.js
@@ -6,7 +6,7 @@ module.exports = (handles, controller, bot) => {
     controller.hears(handles, 'direct_message', function (bot, message) {
         var method = encodeURIComponent(message.match[1]);
         method = method.replace('_', '-');
-        var response = '\`\`\`';
+        
         
         x('http://php.net/manual/en/function.' + method, {
           items: x('body', [{
@@ -15,8 +15,8 @@ module.exports = (handles, controller, bot) => {
           }])
         })(function(err, result) {
             if (err) console.log(err);
-            response += result.items[0].synopsis.replace(/\s\s+/g, ' ');
-            response += result.items[0].description + '\`\`\`';
+            response += "```" +  result.items[0].synopsis.replace(/\s\s+/g, ' ') + "```";
+            response += result.items[0].description;
         });
         
         bot.reply(message, response);
