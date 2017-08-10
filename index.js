96670b5e52bd6236364926c738234c22c841b017
javajohnhub <joedw1978@gmail.com>
2017-08-09 21:33:39 -0400

    ping test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 var bodyParser     = require('body-parser');
+var request = require('request');
 require('dotenv').config();
 var port = process.env.PORT || 5000;
 
@@ -20,6 +21,13 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); //for parsing url encoded
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+setTimeout(function() {
+    request('http://rjdevslack.herokuapp.com', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+}, 60000 * 5);
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running at localhost:' + app.get('port'));
