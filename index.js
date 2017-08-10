2807eb25757b8c2d331a95c31caa11dc60afdf13
javajohnhub <joedw1978@gmail.com>
2017-08-09 21:42:20 -0400

    set int

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); //for parsing url encoded
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-setTimeout(function() {
+setInterval(function() {
     request('http://rjdevslack.herokuapp.com', function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
     });
-}, 1000);
+}, 300000);
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running at localhost:' + app.get('port'));
