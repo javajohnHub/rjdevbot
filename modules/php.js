42e742639df2d88835ced001b35f3e0f5529eb53
javajohnhub <joedw1978@gmail.com>
2017-08-09 20:39:16 -0400

    sewy

--- modules/php.js
+++ modules/php.js
@@ -14,7 +14,7 @@ module.exports = (handles, controller, bot) => {
           }])
         })(function(err, result) {
             if (err) console.log(err);
-            var response =  "`*" + result.items[0].synopsis.replace(/\s\s+/g, ' ') + "*`"
+            var response =  "`" + result.items[0].synopsis.replace(/\s\s+/g, ' ') + "`"
             + "```" + result.items[0].description + "```";
             bot.reply(message, response);
         });
